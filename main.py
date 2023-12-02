2a2da696d764e0b83e6d01a0ebc297e77090e445
ismailhakkii <ismail.kmkl58@gmail.com>
2023-12-02 13:18:26 +0300

    2.12.2023 13.18 tarihli commit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 from tkinter import scrolledtext
+from PIL import Image, ImageTk
+import hashlib
 
-
-# Huffman Kodlama
+# Huffman Ağacı ile Şifreleme ve Deşifreleme
+frekans = []
 
 class DugumAgaci(object):
     def __init__(self, sol=None, sag=None):
@@ -15,6 +17,7 @@ class DugumAgaci(object):
     def __str__(self):
         return '%s_%s' % (self.sol, self.sag)
 
+huffmanKodu = {}
 
 def huffman_kod_agaci(dugum, sol=True, binString=''):
     if type(dugum) is str:
@@ -25,8 +28,18 @@ def huffman_kod_agaci(dugum, sol=True, binString=''):
     d.update(huffman_kod_agaci(r, False, binString + '1'))
     return d
 
-
+def desifrele(huffmanKodu, sifrelenmis_veri):
+    ters_huffmanKodu = {v: k for k, v in huffmanKodu.items()}
+    karakter = ""
+    desifrelenmis_veri = ""
+    for bit in sifrelenmis_veri:
+        karakter += bit
+        if karakter in ters_huffmanKodu:
+            desifrelenmis_veri += ters_huffmanKodu[karakter]
+            karakter = ""
+    return desifrelenmis_veri
 def huffman_hesapla(veri):
+    global huffmanKodu, frekans
     frekans = {}
     for c in veri:
         if c in frekans:
@@ -46,26 +59,39 @@ def huffman_hesapla(veri):
         dugumler = sorted(dugumler, key=lambda x: x[1], reverse=True)
 
     huffmanKodu = huffman_kod_agaci(dugumler[0][0])
-    sonuc = ' Karakter | Frekans | Huffman kodu \n'
-    sonuc += '-----------------------------------\n'
-    for (karakter, frekan) in frekans:
-        sonuc += ' %-4r | %-9d | %s\n' % (karakter, frekan, huffmanKodu[karakter])
-
     sifrelenmis_veri = ''.join([huffmanKodu[karakter] for karakter in veri])
-    sonuc += '-----------------------------------\n'
-    sonuc += 'Şifrelenmiş Veri: ' + sifrelenmis_veri
+    return sifrelenmis_veri
+def desifrele_veri():
+    sifrelenmis_veri = desifre_metin_giris.get("1.0", 'end-1c')
+    sonuc = desifrele(huffmanKodu, sifrelenmis_veri)
+    sonuc_text = ' Karakter | Frekans \n'
+    sonuc_text += '---------------------\n'
+    for (karakter, frekan) in frekans:
+        sonuc_text += ' %-4r | %-9d \n' % (karakter, frekan)
+    sonuc_text += '---------------------\n'
+    sonuc_text += 'Deşifrelenmiş Veri: ' + sonuc
+    desifre_metin_giris.delete(1.0, tk.END)
+    desifre_metin_giris.insert(tk.END, sonuc_text)
 
-    return sonuc
+
+def md5_sifrele(veri):
+    md5_obj = hashlib.md5()
+    md5_obj.update(veri.encode('utf-8'))
+    return md5_obj.hexdigest()
 
 
 def veri_gonder():
     veri = metin_giris.get("1.0", 'end-1c')
     giris_cerceve.pack_forget()
     cikti_cerceve.pack(padx=20, pady=20)
-    sonuc = huffman_hesapla(veri)
+
+    sifrelenmis_veri = huffman_hesapla(veri)
+    md5_sonuc = md5_sifrele(sifrelenmis_veri)
+
+    sonuc = "Huffman ile Şifrelenmiş Veri:\n" + sifrelenmis_veri + "\n\nHuffman ve MD5 ile Şifrelenmiş Veri:\n" + md5_sonuc
+
     metin_cikti.delete(1.0, tk.END)
     metin_cikti.insert(tk.END, sonuc)
-
 
 def geri_dön():
     giris_cerceve.pack_forget()
@@ -89,23 +115,54 @@ def sifreleme_ekrani():
     menu_cerceve.pack_forget()
     giris_cerceve.pack(padx=20, pady=20)
 
+pencere = tk.Tk()
+pencere.title("Huffman Şifreleme")
+def karakter_frekans_ekrani():
+    cikti_cerceve.pack_forget()
+    frekans_cerceve.pack(padx=20, pady=20)
+
+    frekans_text = ' Karakter | Frekans \n'
+    frekans_text += '---------------------\n'
+    for (karakter, frekan) in frekans:
+        frekans_text += ' %-4r | %-9d \n' % (karakter, frekan)
+
+    frekans_metin_cikti.delete(1.0, tk.END)
+    frekans_metin_cikti.insert(tk.END, frekans_text)
+
+
+frekans_cerceve = tk.Frame(pencere, padx=10, pady=10)
+frekans_metin_cikti = scrolledtext.ScrolledText(frekans_cerceve, wrap=tk.WORD, width=40, height=10)
+frekans_metin_cikti.pack()
+
+geri_buton_frekans = tk.Button(frekans_cerceve, text="Geri", command=ana_menu)
+geri_buton_frekans.pack(side=tk.LEFT, padx=10)
+
+cikis_buton_frekans = tk.Button(frekans_cerceve, text="Çıkış", command=cikis_yap)
+cikis_buton_frekans.pack(side=tk.RIGHT, padx=10)
 
 def desifreleme_ekrani():
     menu_cerceve.pack_forget()
     desifre_cerceve.pack(padx=20, pady=20)
 
 
-pencere = tk.Tk()
-pencere.title("Huffman Şifreleme")
+
 
 menu_cerceve = tk.Frame(pencere, padx=10, pady=10)
 menu_cerceve.pack(padx=20, pady=20)
 
+#Huffman ağacı resmi ekledik resmi Yapay Zeka ile oluşturduk.
+resim_yolu = "OIG.jfif"
+resim = Image.open(resim_yolu)
+resim = resim.resize((150, 150))
+tk_resim = ImageTk.PhotoImage(resim)
+resim_etiketi = tk.Label(menu_cerceve, image=tk_resim)
+resim_etiketi.grid(row=0, column=0, columnspan=2, pady=20)
+
 sifrele_buton = tk.Button(menu_cerceve, text="Şifrele", command=sifreleme_ekrani)
-sifrele_buton.grid(row=0, column=0, padx=10)
+sifrele_buton.grid(row=1, column=0, padx=10)
 
 desifrele_buton = tk.Button(menu_cerceve, text="Deşifre", command=desifreleme_ekrani)
-desifrele_buton.grid(row=0, column=1, padx=10)
+desifrele_buton.grid(row=1, column=1, padx=10)
 
 giris_cerceve = tk.Frame(pencere, padx=10, pady=10)
 etiket_giris = tk.Label(giris_cerceve, text="Şifrelenecek metni giriniz:")
@@ -123,8 +180,11 @@ cikis_buton = tk.Button(giris_cerceve, text="Çıkış", command=cikis_yap)
 cikis_buton.pack(side=tk.RIGHT, padx=10)
 
 cikti_cerceve = tk.Frame(pencere, padx=10, pady=10)
+
 metin_cikti = scrolledtext.ScrolledText(cikti_cerceve, wrap=tk.WORD, width=40, height=10)
 metin_cikti.pack()
+devam_buton_cikti = tk.Button(cikti_cerceve, text="Karakter ve Frekans", command=karakter_frekans_ekrani)
+devam_buton_cikti.pack(pady=10)
 
 geri_buton_cikti = tk.Button(cikti_cerceve, text="Geri", command=geri_dön)
 geri_buton_cikti.pack(side=tk.LEFT, padx=10)
@@ -136,7 +196,8 @@ desifre_cerceve = tk.Frame(pencere, padx=10, pady=10)
 desifre_metin_giris = scrolledtext.ScrolledText(desifre_cerceve, wrap=tk.WORD, width=40, height=10)
 desifre_metin_giris.pack()
 
-desifrele_buton = tk.Button(desifre_cerceve, text="Metni Deşifrele", command=veri_gonder)
+desifrele_buton = tk.Button(desifre_cerceve, text="Metni Deşifrele", command=desifrele_veri)
+
 desifrele_buton.pack(pady=10)
 
 geri_buton_desifre = tk.Button(desifre_cerceve, text="Geri", command=ana_menu)
