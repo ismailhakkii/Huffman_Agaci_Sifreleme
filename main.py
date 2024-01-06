24b10acfbd937d5a5111b4973a7838086e1d4b2e
ismailhakkii <ismail.kmkl58@gmail.com>
2024-01-06 16:00:56 +0300

    metni şifrele tuşuna bir kez basıldıktan sonra tuş gizleniyor. 6012024

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import tkinter as tk
 from tkinter import scrolledtext
 from PIL import Image, ImageTk
-import hashlib
-from  math import  ceil
+from math import ceil
+from sympy import symbols, diff
+import random
 
-# Huffman Ağacı ile Sıkıştırma, Şifreleme ve Deşifreleme
+# Global değişkenlerimiz
+ozel_alfabe = "abcçdefgğhıijklmnoöpqrsştuüvwxyzABCÇDEFGĞHIİJKLMNOÖQRSŞTUÜWVXYZ.,:;-+!?/*1234567890 "
 frekans = []
+kaydirma = None
+
+# x sembolünü ve fonksiyonları tanımladık
+x = symbols('x')
+fonksiyonlar = [4 * x ** 3, 2 * x ** 4 + 3 * x + 5, x ** 4 - 6 * x ** 4 + 8]
+
+
+def kaydirma_degeri_hesapla():
+    rastgele_x_degeri = random.randint(1, 100)
+    secilen_fonksiyon = random.choice(fonksiyonlar)
+    turev = diff(secilen_fonksiyon, x, 2)
+    sonuc = turev.subs(x, rastgele_x_degeri)
+    alfabe_uzunlugu = len(ozel_alfabe)
+    return int(sonuc % alfabe_uzunlugu)
+
+
+kaydirma = kaydirma_degeri_hesapla()
+
 
 class DugumAgaci(object):
     def __init__(self, sol=None, sag=None):
@@ -18,7 +38,9 @@ class DugumAgaci(object):
     def __str__(self):
         return '%s_%s' % (self.sol, self.sag)
 
+
 huffmanKodu = {}
+
 
 def huffman_kod_agaci(dugum, sol=True, binString=''):
     if type(dugum) is str:
@@ -29,16 +51,7 @@ def huffman_kod_agaci(dugum, sol=True, binString=''):
     d.update(huffman_kod_agaci(r, False, binString + '1'))
     return d
 
-def desifrele(huffmanKodu, sifrelenmis_veri):
-    ters_huffmanKodu = {v: k for k, v in huffmanKodu.items()}
-    karakter = ""
-    desifrelenmis_veri = ""
-    for bit in sifrelenmis_veri:
-        karakter += bit
-        if karakter in ters_huffmanKodu:
-            desifrelenmis_veri += ters_huffmanKodu[karakter]
-            karakter = ""
-    return desifrelenmis_veri
+
 def huffman_hesapla(veri):
     global huffmanKodu, frekans
     frekans = {}
@@ -68,61 +81,77 @@ def huffman_hesapla(veri):
     tasarruf_yuzdesi = ((original_bit_size - compressed_bit_size) / original_bit_size) * 100
 
     return sifrelenmis_veri, original_bit_size, compressed_bit_size, tasarruf_yuzdesi
+
+
 original_bit_size = None
 compressed_bit_size = None
 tasarruf_yuzdesi = None
 
-def desifrele_veri():
-    global original_bit_size, compressed_bit_size, tasarruf_yuzdesi
 
+def ozel_sifrele(metin):
+    global kaydirma
+    sifrelenmis_metin = ""
+    for karakter in metin:
+        if karakter in ozel_alfabe:
+            yeni_index = (ozel_alfabe.index(karakter) + kaydirma) % len(ozel_alfabe)
+            sifrelenmis_metin += ozel_alfabe[yeni_index]
+        else:
+            sifrelenmis_metin += karakter
+    return sifrelenmis_metin
+
+
+def veri_gonder():
+    global original_bit_size, compressed_bit_size, tasarruf_yuzdesi
+    veri = metin_giris.get("1.0", 'end-1c')
+    veri = veri.replace('\xa0', ' ')  # NBSP'yi kaldırır
+    giris_cerceve.pack_forget()
+    cikti_cerceve.pack(padx=20, pady=20)
+
+    sifrelenmis_veri = ozel_sifrele(veri)
+
+    sifrelenmis_veri_huffman, original_bit_size, compressed_bit_size, tasarruf_yuzdesi = huffman_hesapla(
+        sifrelenmis_veri)
+    byte = ceil(original_bit_size / 8)
+    compressed_byte = ceil(compressed_bit_size / 8)
+
+    sonuc = f"Huffman ile Şifrelenmiş Veri:\n{sifrelenmis_veri_huffman}\n"
+    sonuc += f"Orijinal Veri Boyutu: {original_bit_size} bit ve {byte} byte  \n"
+    sonuc += f"Sıkıştırılmış Veri Boyutu: {compressed_bit_size} bit ve {compressed_byte} byte \n"
+    sonuc += f"Veri Tasarrufu: %{tasarruf_yuzdesi:.2f}\n"
+    metin_cikti.delete(1.0, tk.END)
+    metin_cikti.insert(tk.END, sonuc)
+
+
+def desifrele_veri():
+    global huffmanKodu, frekans, original_bit_size, compressed_bit_size, tasarruf_yuzdesi
+    desifrele_buton.pack_forget()
+
+
+    # Şifrelenmiş veriyi kullanıcı girişinden alıyoruz
     sifrelenmis_veri = desifre_metin_giris.get("1.0", 'end-1c')
-    sonuc = desifrele(huffmanKodu, sifrelenmis_veri)
+
+    # Deşifreleme işlemini yapıp ve ters Huffman kodunu kullanarak deşifrelenmiş veriyi elde ettik
+    ters_huffmanKodu = {v: k for k, v in huffmanKodu.items()}
+    karakter = ""
+    desifrelenmis_veri = ""
+    for bit in sifrelenmis_veri:
+        karakter += bit
+        if karakter in ters_huffmanKodu:
+            desifrelenmis_veri += ters_huffmanKodu[karakter]
+            karakter = ""
+
     sonuc_text = ' Karakter | Frekans \n'
     sonuc_text += '---------------------\n'
     for (karakter, frekan) in frekans:
         sonuc_text += ' %-4r | %-9d \n' % (karakter, frekan)
     sonuc_text += '---------------------\n'
-    sonuc_text += 'Deşifrelenmiş Veri: ' + sonuc + '\n'
+    sonuc_text += 'Deşifrelenmiş Veri: ' + desifrelenmis_veri + '\n'
 
-    if original_bit_size is not None and compressed_bit_size is not None and tasarruf_yuzdesi is not None:
-        sonuc_text += f"\nOrijinal Veri Boyutu: {original_bit_size} bit\n"
-        sonuc_text += f"Sıkıştırılmış Veri Boyutu: {compressed_bit_size} bit\n"
-        sonuc_text += f"Veri Tasarrufu: %{tasarruf_yuzdesi:.2f}\n"
-
-    desifre_metin_giris.delete(1.0, tk.END)
-    desifre_metin_giris.insert(tk.END, sonuc_text)
-    desifrele_buton.pack_forget()
+    desifre_metin_giris.delete("1.0", tk.END)
+    desifre_metin_giris.insert("1.0", sonuc_text)
 
 
-def md5_sifrele(veri):
-    md5_obj = hashlib.md5()
-    md5_obj.update(veri.encode('utf-8'))
-    return md5_obj.hexdigest()
-
-
-def veri_gonder():
-    veri = metin_giris.get("1.0", 'end-1c')
-    veri = veri.replace('\xa0', ' ')  # NBSP'yi kaldır
-
-    giris_cerceve.pack_forget()
-    cikti_cerceve.pack(padx=20, pady=20)
-
-    sifrelenmis_veri, original_bit_size, compressed_bit_size, tasarruf_yuzdesi = huffman_hesapla(veri)
-    md5_sonuc = md5_sifrele(sifrelenmis_veri)
-    byte=ceil(original_bit_size/8)
-    compressed_byte=ceil(compressed_bit_size/8)
-
-    sonuc = f"Huffman ile Şifrelenmiş Veri:\n{sifrelenmis_veri}\n"
-    sonuc += f"Huffman ve MD5 ile Şifrelenmiş Veri:\n{md5_sonuc}\n\n"
-    sonuc += f"Orijinal Veri Boyutu: {original_bit_size} bit ve {byte} byte  \n"
-    sonuc += f"Sıkıştırılmış Veri Boyutu: {compressed_bit_size} bit ve {compressed_byte} byte \n"
-    sonuc += f"Veri Tasarrufu: %{tasarruf_yuzdesi:.2f}\n"
-
-    metin_cikti.delete(1.0, tk.END)
-    metin_cikti.insert(tk.END, sonuc)
-
-
-def geri_dön():
+def geri_don():
     giris_cerceve.pack_forget()
     cikti_cerceve.pack_forget()
     desifre_cerceve.pack_forget()
@@ -145,8 +174,14 @@ def sifreleme_ekrani():
     menu_cerceve.pack_forget()
     giris_cerceve.pack(padx=20, pady=20)
 
+
 pencere = tk.Tk()
 pencere.title("Huffman Şifreleme")
+
+
+def alfabeyi_otele(kaydirma, ozel_alfabe):
+    return ozel_alfabe[kaydirma:] + ozel_alfabe[:kaydirma]
+
 
 def karakter_frekans_ekrani():
     cikti_cerceve.pack_forget()
@@ -156,6 +191,10 @@ def karakter_frekans_ekrani():
     frekans_text += '---------------------\n'
     for (karakter, frekan) in frekans:
         frekans_text += ' %-4r | %-9d \n' % (karakter, frekan)
+
+    otelemis_alfabe = alfabeyi_otele(kaydirma, ozel_alfabe)
+    frekans_text += '---------------------\n'
+    frekans_text += f"Ötelenmiş Alfabe: {otelemis_alfabe}\n"
 
     frekans_metin_cikti.delete(1.0, tk.END)
     frekans_metin_cikti.insert(tk.END, frekans_text)
@@ -171,6 +210,7 @@ geri_buton_frekans.pack(side=tk.LEFT, padx=10)
 cikis_buton_frekans = tk.Button(frekans_cerceve, text="Çıkış", command=cikis_yap)
 cikis_buton_frekans.pack(side=tk.RIGHT, padx=10)
 
+
 def desifreleme_ekrani():
     menu_cerceve.pack_forget()
     desifre_cerceve.pack(padx=20, pady=20)
@@ -179,7 +219,7 @@ def desifreleme_ekrani():
 menu_cerceve = tk.Frame(pencere, padx=10, pady=10)
 menu_cerceve.pack(padx=20, pady=20)
 
-#Huffman ağacı resmi ekledik resmi Yapay Zeka ile oluşturduk.
+# Huffman ağacı resmi ekledik resmi Yapay Zeka ile oluşturduk.
 resim_yolu = "huffmant_ree.png"
 resim = Image.open(resim_yolu)
 resim = resim.resize((225, 225))
@@ -202,7 +242,7 @@ metin_giris.pack()
 sifrele_buton = tk.Button(giris_cerceve, text="Metni Şifrele", command=veri_gonder)
 sifrele_buton.pack(pady=10)
 
-geri_buton = tk.Button(giris_cerceve, text="Geri", command=geri_dön)
+geri_buton = tk.Button(giris_cerceve, text="Geri", command=geri_don)
 geri_buton.pack(side=tk.LEFT, padx=10)
 
 cikis_buton = tk.Button(giris_cerceve, text="Çıkış", command=cikis_yap)
@@ -215,7 +255,7 @@ metin_cikti.pack()
 devam_buton_cikti = tk.Button(cikti_cerceve, text="Karakter ve Frekans", command=karakter_frekans_ekrani)
 devam_buton_cikti.pack(pady=10)
 
-geri_buton_cikti = tk.Button(cikti_cerceve, text="Geri", command=geri_dön)
+geri_buton_cikti = tk.Button(cikti_cerceve, text="Geri", command=geri_don)
 geri_buton_cikti.pack(side=tk.LEFT, padx=10)
 
 cikis_buton_cikti = tk.Button(cikti_cerceve, text="Çıkış", command=cikis_yap)
@@ -225,7 +265,7 @@ desifre_cerceve = tk.Frame(pencere, padx=10, pady=10)
 desifre_metin_giris = scrolledtext.ScrolledText(desifre_cerceve, wrap=tk.WORD, width=40, height=10)
 desifre_metin_giris.pack()
 
-desifrele_buton = tk.Button(desifre_cerceve, text="Metni Deşifrele", command=desifrele_veri)
+desifrele_buton = tk.Button(desifre_cerceve, text="Veriyi Deşifrele", command=desifrele_veri)
 
 desifrele_buton.pack(pady=10)
 
